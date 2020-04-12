8d499115066d231cc428da7ff394c82c5bab87dd
cuijunsheng <744612262@qq.com>
2020-04-12 21:31:18 +0800

    首页开发：导航栏的封装和使用

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
-      <h2>首页</h2>
-    </div>
+  <div id="home">
+    <nav-bar class="home-nav-bar">
+      <div slot="left">left</div>
+      <div slot="center">购物街</div>
+      <div slot="right">right</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from 'components/common/navbar/NavBar'
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
     }
+  }
 </script>
 
 <style scoped>
-
+.home-nav-bar{
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
