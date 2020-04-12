653bf857a3fd7f075a6eaba271e2868d84aca3d5
cuijunsheng <744612262@qq.com>
2020-04-12 17:32:12 +0800

    '添加自定义配置vue.config.js,并配置目录别名'

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+/**
+ * vue.config.js是vue-cli3以上版本，提供给我们自己定义配置的文件，运行和打包时会将该文件与基本的配置文件合并
+ * @type {{}}
+ */
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets':'@/assets',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network',
+        'views':'@/views'
+      }
+    }
+  }
+}
