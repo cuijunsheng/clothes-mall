d84698c6e6509d8151f8149e8b24c543befcf6fc
cuijunsheng <744612262@qq.com>
2020-04-13 21:59:59 +0800

    首页开发：轮播图的展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,27 +1,40 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav-bar">
-      <div slot="left">left</div>
-      <div slot="center">购物街</div>
-      <div slot="right">right</div>
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './components/HomeSwiper'
+
+  import {getHomeMultiData} from "network/home";
 
   export default {
     name: "Home",
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper
+    },
+    created() {
+      getHomeMultiData().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
     }
   }
 </script>
 
 <style scoped>
-.home-nav-bar{
-  background-color: var(--color-tint);
-  color: #fff;
-}
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
