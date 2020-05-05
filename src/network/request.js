aa01aa431463895e2fab49cdb5466f5c51c12d9d
cuijunsheng <744612262@qq.com>
2020-05-05 21:01:23 +0800

    请求接口问题

--- src/network/request.js
+++ src/network/request.js
@@ -6,7 +6,7 @@ export function request(config) {
    * 1.创建axios实例
    */
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:8000/api/h8',
+    baseURL: '111111111111',
     timeout: 5000
   })
 
